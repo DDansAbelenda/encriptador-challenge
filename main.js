0c45cf3d55aeaa00a5fcb991f09f9c70d4f4d44d
Daniel Dans Abelenda <ddansabelenda@gmail.com>
2024-02-20 21:59:35 -0500

    se arregla la forma de desencriptar las cadenas de texto, se agrega además una carpeta al gitignore

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ const encriptador = {
     "u": "ufat"
 };
 
+const desencriptador = {
+    "enter": "e",
+    "imes": "i",
+    "ai": "a",
+    "ober": "o",
+    "ufat": "u"
+}
+
 const regex = /[A-ZÁÉÍÓÚáéíóú+*·'¨^]/;
 
 /*Agregando eventos a los botones*/
@@ -42,15 +50,21 @@ function encriptar() {
 function desencriptar() {
     let texto = document.getElementById("input").value;
     if (!regex.test(texto)) {
-        for (let letra in encriptador) {
-            texto = texto.replaceAll(encriptador[letra], letra);
+        let tempArray = dividirCadena(texto);
+        texto = "";
+        for (let cadena of tempArray) {
+            if (desencriptador[cadena]) {
+                texto += desencriptador[cadena];
+            }
+            else {
+                texto += cadena
+            }
         }
         escribirSalida(texto);
         borrarEntrada();
     } else {
         alert("El texto no debe contener mayúsculas, acentos ni caracteres especiales");
     }
-
 }
 
 function copiar() {
@@ -78,4 +92,56 @@ function borrarEntrada() {
     document.getElementById("input").value = "";
 }
 
+/**
+ * Función auxiliar para el proceso de desencriptar. Su procedimiento consiste en dividir el texto
+ * recibido por parámetros y devolverlo en un arreglo. La división se hace a partir de las subcadenas 
+ * encriptadas detectadas. Ejemplo para el texto: jaimenters -> return [j, ai, m, enter, s].
+ * @param {*} texto cadena que se divide
+ * @returns arreglo con el texto recibido dividio
+ */
+function dividirCadena(texto) {
+    let tempArray = []
+    for (let letra in encriptador) {
+        //Caso inicial
+        if (letra === "e") {
+            tempArray = texto.split(encriptador[letra]);
+            tempArray = completarArreglo(tempArray, encriptador[letra]);
+        } else {
+            //Resto de los casos
+            tempArray = tempArray.map((text) => {
+                text = text.split(encriptador[letra]);
+                text = completarArreglo(text, encriptador[letra]);
+                return text;
+            }).flat();
+        }
+    }
+    return tempArray;
+}
+
+
+/**
+ * Función auxiliar para el proceso de desencriptar. Su procedimiento consiste en 
+ * agregar la cadena recibida por parámetros en el arreglo de la siguiente forma:
+ * Ejemplo: Sea arreglo = [a,b,b,c] y la cadena m, el arreglo resultante sería: [a,m,b,m,b,m,c]
+ * 
+ * @param {*} arreglo array al que se le agrega la cadena
+ * @param {*} cadena cadena que se agrega luego de cada posición excepto la última
+ * @returns arreglo con la cadena insertada
+ */
+function completarArreglo(arreglo, cadena) {
+    let tempArray = [];
+    if (arreglo.length !== 1) {
+        let j = 0;
+        for (let i = 0; i < arreglo.length - 1; i++) {
+            tempArray[j] = arreglo[i];
+            tempArray[j + 1] = cadena;
+            j += 2;
+        }
+        tempArray[j] = arreglo[arreglo.length - 1];
+
+    } else {
+        tempArray = arreglo;
+    }
+    return tempArray;
+}
 
